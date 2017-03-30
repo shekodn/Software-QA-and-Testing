27aad7c5b5b70fa4ebc7b3ce132363a2cec4062f
shekodn <sdiazn19@gmail.com>
2017-03-29 19:40:34 -0600

    removes gamma class

--- lab/5/programa 5/TDist.java
+++ lab/5/programa 5/TDist.java
@@ -4,6 +4,49 @@ import java.util.*;
 
 public class TDist {
 
+
+	//&i
+	public static double Factorial(int n){
+		double ifact = 1;
+		for (int i = 1; i <= n; i++) {
+			ifact *= i;
+		}
+		return ifact;
+	}
+
+	//&i
+	public static Double Calculo(Double value){
+		ArrayList<Double> valores = new ArrayList<Double>();
+		Double dTotal = 1.0;
+		do{
+			valores.add(value);
+			value-= 1;
+		}while(value > 0);
+		for(int i= 0; i < valores.size(); i++){
+			dTotal *= valores.get(i);
+		}
+		return dTotal * Math.sqrt(Math.PI);
+	}
+
+	//&i
+	//&b=9
+	public double calculaGama(double x){
+		if (x == 0){
+			return -1;
+		}
+		if (x == 1.0){
+			return 1;
+		}
+		if (x == 0.5){
+
+			return Math.sqrt(Math.PI);
+		}
+		else{
+			return (x - 1) * calculaGama(x - 1);
+		}
+	}
+
+
 	//&i
 	public ArrayList<Double> TDistOne(ArrayList<Double> x,Double dof){
 		ArrayList<Double> distOne = new ArrayList<Double>();
@@ -32,19 +75,19 @@ public class TDist {
 		String sDof = String.valueOf(Impresion.Formatear("##.#####", ivalue));
 		try{
 			int iValue = Integer.parseInt(sDof);
-			dValueUp = (double) Gamma.Factorial(iValue-1);
+			dValueUp = (double) Factorial(iValue-1);
 		}catch(NumberFormatException e){
 			double dValue = Double.parseDouble(sDof);
-			dValueUp = Gamma.Calculo(dValue-1);
+			dValueUp = Calculo(dValue-1);
 		}
 		ivalue = dof/2.0;
 		sDof = String.valueOf(Impresion.Formatear("##.#####", ivalue));
 		try{
 			int iValue = Integer.parseInt(sDof);
-			dValueDownTwo = (double) Gamma.Factorial(iValue-1);
+			dValueDownTwo = (double) Factorial(iValue-1);
 		}catch(NumberFormatException e){
 			double dValue = Double.parseDouble(sDof);
-			dValueDownTwo = Gamma.Calculo(dValue-1);
+			dValueDownTwo = Calculo(dValue-1);
 		}
 
 		Double dValueDownOne = Math.pow((dof * Math.PI), 0.5);
